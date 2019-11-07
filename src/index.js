0789eac47083ad60f499c6b1a365355ece5f4dc5
Eivin Adlawan <eadlawa2@gmail.com>
2019-11-07 12:34:22 -0600

    Savepoint: syntax errors fixed

--- src/index.js
+++ src/index.js
@@ -3,11 +3,13 @@ import ReactDOM from 'react-dom';
 import App from './components/App.js';
 import searchYouTube from './lib/searchYouTube.js';
 import YOUTUBE_API_KEY from './config/youtube.js';
+import Store from './store/store.js' //added to allow Store to be used throughout the entire app
+import { Provider } from 'react-redux'; //added to force Store to be available throughout the entire app
 
 //TODO: Import the Provider component from 'react-redux' here!
 
-//TODO: Use the Provider component to make your store available to
-//  the rest of your app.
+//TODO: Use the Provider component to make your store available to the rest of your app.
+//! Read the article about Provider
 
 ReactDOM.render(
   <App API_KEY={YOUTUBE_API_KEY} searchYouTube={searchYouTube} />,
