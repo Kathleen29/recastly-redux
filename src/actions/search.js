b3fa3a321bb585ab16e766774c2adb489693296c
Kathleen Trainor <kathleentrainor9@gmail.com>
2019-11-06 23:04:16 -0500

    Set up store and begin handleVideoSearch action

--- src/actions/search.js
+++ src/actions/search.js
@@ -5,18 +5,41 @@ import YOUTUBE_API_KEY from '../config/youtube.js';
 
 
 var handleVideoSearch = (q) => {
-
+  var options = {
+    key: YOUTUBE_API_KEY,
+    query: q
+};
   //TODO:  Write an asynchronous action to handle a video search!
+  // searchYouTube(options).dispatch(type, videos)`
+  var videos = searchYouTube(options, function(){console.log(items)});
+  console.log(videos);
 
-  var videos = searchYouTube(YOUTUBE_API_KEY, q);
-  {
-      setTimeout(function(videos) {
-        dispatch ({
+
+       return ({
           type: 'CHANGE_VIDEO',
           videos: videos
         });
-      }, 100);
-    };
+
+
+
+
+
+
+        // return  changeVideo(videos)
+
+
+        // var changeVideo = (video) => ({
+
+        //   type: 'CHANGE_VIDEO',
+        //   video: video
+
+        //   //have the changeVideo action to change store.state
+        // });
+
+
 };
+
+
+
 
 export default handleVideoSearch;
