a274ea8663bffe5ccd9f2c1dec20f2df2f3a269a
Kathleen Trainor <kathleentrainor9@gmail.com>
2019-11-06 20:31:02 -0500

    Implement actions and begin reducers

--- src/actions/search.js
+++ src/actions/search.js
@@ -5,8 +5,18 @@ import YOUTUBE_API_KEY from '../config/youtube.js';
 
 
 var handleVideoSearch = (q) => {
- 
+
   //TODO:  Write an asynchronous action to handle a video search!
+
+  var videos = searchYouTube(YOUTUBE_API_KEY, q);
+  {
+      setTimeout(function(videos) {
+        dispatch ({
+          type: 'CHANGE_VIDEO',
+          videos: videos
+        });
+      }, 100);
+    };
 };
 
 export default handleVideoSearch;
--- src/reducers/currentVideo.js
+++ src/reducers/currentVideo.js
@@ -2,6 +2,12 @@ import Redux from 'redux';
 
 var currentVideoReducer = (state, action) => {
   //TODO: define a reducer for the currentVideo field of our state.
-};
-
+  if(action.type === 'CHANGE_VIDEO') {
+    console.log('yes!');
+  }
+  if(!state) {
+    state.default = null;
+  }
+}
 export default currentVideoReducer;
+
