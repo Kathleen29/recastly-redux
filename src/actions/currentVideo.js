bf2f7190ebc70583912b20446c7b88500e990619
Eivin Adlawan <eadlawa2@gmail.com>
2019-11-07 17:56:12 -0600

    Savepoint: action/currentVideo is now working, containting a type and a payload. We have also a reducers/currentVideo which is given two arguments, the current state and the action: we are using 'switch' to FLAG what we are looking for, and for this reducer we are looking for the action.type. Then the 'case' we are looing for is the 'CHANGE_VIDEO', which is the value of that switch. When it is found, it will return the action.video or null (just in case action.video has no contents. The default state is set to 'null' so that we are explicitly telling the user, "Hey, there's no video here," rather than throwing an undefined, which can be caused by syntax errors, etc.

--- src/actions/currentVideo.js
+++ src/actions/currentVideo.js
@@ -1,5 +1,6 @@
 var changeVideo = (video) => ({
   //TODO:  Return some action object to change the currently playing video.
+  //! Note: every ACTION requires two arguments a 'type' (this is REQUIRED for every avtion) and the value is a string (it can be anything to describe this payload data). 'Payload key' and the value you are passing, which is the argument; here it is 'video'.
   //should return an object with a "type" key equal to "CHANGE_VIDEO"
   type: 'CHANGE_VIDEO',
   video: video
--- src/reducers/currentVideo.js
+++ src/reducers/currentVideo.js
@@ -1,13 +1,20 @@
 import Redux from 'redux';
 
-var currentVideoReducer = (state, action) => {
-  //TODO: define a reducer for the currentVideo field of our state.
-  if(action.type === 'CHANGE_VIDEO') {
+var currentVideoReducer = (state = null, action) => { //you can add a default value to the 'state' and assign it with 'null
+  //DONE: define a reducer for the currentVideo field of our state. What this does is that based on an 'action' and the current state, it will return a new state or the current state.
+  //! 02:00 Reducers create new states or return existing state for a particular entity
+  // if(action.type === 'CHANGE_VIDEO') {
     console.log('yes!');
+    switch (action.type) {
+    case 'CHANGE_VIDEO':
+      return action.video || null; //! this is the NEW state. If action.video does not contain a video, instead of returning 'undefined', it will return null.
+    default:
+      return state;
+    }
   }
-  if(!state) {
-    state.default = null;
-  }
-}
+  // if(!state) {
+  //   state.default = null;
+  // }
+// }
 export default currentVideoReducer;
 
